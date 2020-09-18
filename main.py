b724f0aa24900323b2c5261c35d379316c410d07
nickstpierre <55991065+nickstpierre@users.noreply.github.com>
2020-09-17 20:32:22 -0400

    Add files via upload
    Files necessary for the simulation program.

--- main.py
+++ main.py
@@ -1,28 +1,51 @@
 """
 Author: Nick St. Pierre
 Filename: main.py
-Description: File used for testing the motor methods.
+Description: Program used to simulate the decision algorithm for Mercury Robot.
 """
+import pygame, os, random
+from modules.vector2D import Vector2
+from modules.drawable import Drawable
+from modules.obstacle import Obstacle
+from modules.robot import Robot
 
-from automobile import Automobile
-from motor import Motor
-import pygame
 
-SCREEN_SIZE = (500,500)
+SCREEN_SIZE = Vector2(800, 550)
+WORLD_SIZE = Vector2(800, 550)
 
 def main():
-    
-    RUNNING = True
-    pygame.init()
-    pygame.display.set_caption("SERVO TEST")
-    screen = pygame.display.set_mode(list(SCREEN_SIZE))
-    
-    robot = Automobile(Motor("GPIO17"),
-                       Motor("GPIO27"),
-                       Motor("GPIO22"),
-                       Motor("GPIO23"))
-    
-    while RUNNING:
+   # initialize the pygame module
+   pygame.init()
+   
+   # load and set the logo
+   pygame.display.set_caption("Mercury Simulation")
+   
+   # initialize the screen
+   screen = pygame.display.set_mode(list(SCREEN_SIZE))
+      
+   obstacleList = []
+   robot = Robot(Vector2(360,450))
+   
+   # Let's make a background so we can see if we're moving
+   background = Drawable(Vector2(0,0), "background.png", offset = None)
+   
+   # Initialize the gameClock for more realistic velocity
+   gameClock = pygame.time.Clock()
+   
+   # define a variable to control the main loop
+   RUNNING = True
+   
+   # main loop
+   while RUNNING:
+      
+      # Draw everything, adjust by offset
+      background.draw(screen)
+      robot.draw(screen)
+      for obstacle in obstacleList:
+         obstacle.draw(screen)
+      
+      # Flip the display to the monitor
+      pygame.display.flip()
       
       # event handling, gets all event from the eventqueue
       for event in pygame.event.get():
@@ -30,19 +53,34 @@ def main():
          if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             # change the value to False, to exit the main loop
             RUNNING = False
-         elif event.type == pygame.KEYDOWN:
-               if event.key == pygame.K_UP:
-                  # change to up arrow
-                  robot.drive()
-               elif event.key == pygame.K_DOWN:
-                  robot.reverse()
-               elif event.key == pygame.K_RIGHT:
-                  robot.turnRight()
-               elif event.key == pygame.K_LEFT:
-                  robot.turnLeft()
-         elif event.type == pygame.KEYUP:
-            robot.park()       
-    pygame.quit()
-    
-if __name__ == '__main__':
-    main()
+         
+         robot.handleEvent(event)
+         
+         if event.type == pygame.MOUSEBUTTONDOWN:
+            adjustedPos = list([int(x) for x in event.pos])
+            obstacleList.append(Obstacle(Vector2(*background.adjustMousePos(adjustedPos)), "sidewall.png", SCREEN_SIZE))
+            # if event.key == pygame.K_S:
+            #     obstacleList.append(Obstacle(Vector2(*background.adjustMousePos(adjustedPos)), "sidewall.png", SCREEN_SIZE))
+            # elif event.key == pygame.K_T:
+            #     obstacleList.append(Obstacle(Vector2(*background.adjustMousePos(adjustedPos)), "topwall.png", SCREEN_SIZE))
+         
+      gameClock.tick(60)
+      
+      ticks = gameClock.get_time() / 1000
+            
+      
+      # Update everything
+      robot.update(WORLD_SIZE, ticks)
+      for obstacle in obstacleList:         
+         if robot.getCollideRect().colliderect(obstacle.getCollideRect()):
+            print("You hit the wall")
+      
+      # Update the camera
+      background.updateOffset(robot, SCREEN_SIZE, WORLD_SIZE)
+      
+
+   pygame.quit()
+   
+   
+if __name__ == "__main__":
+   main()
